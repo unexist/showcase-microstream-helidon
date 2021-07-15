cb2d01a9d00a9066cd96d32e0f90c3366743ae9a
Christoph Kappel <christoph@unexist.dev>
2021-07-15 12:31:33 +0200

    Set repository implementation to microstream

--- src/main/java/dev/unexist/showcase/todo/infrastructure/persistence/ListTodoRepository.java
+++ src/main/java/dev/unexist/showcase/todo/infrastructure/persistence/ListTodoRepository.java
@@ -17,12 +17,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
+@Named(value = "list")
 public class ListTodoRepository implements TodoRepository {
     private static final Logger LOGGER = LoggerFactory.getLogger(ListTodoRepository.class);
 
